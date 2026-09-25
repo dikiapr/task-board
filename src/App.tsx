5f38c68194b5531e24847f7643f45c35308371c5
diki <adiki436@gmail.com>
2026-09-25 11:24:21 +0700

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -28,10 +27,11 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+// import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import KanbanPage from './pages/KanbanPage';
 
 setupIonicReact();
 
@@ -39,8 +39,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/kanban" element={<KanbanPage />} />
+        <Route path="*" element={<Navigate to="/kanban" replace />} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
---
+++ src/pages/KanbanPage.tsx
@@ -0,0 +1,9 @@
+import { IonContent, IonPage } from '@ionic/react';
+
+   const KanbanPage = () => (
+     <IonPage>
+       <IonContent>Halo Kanban</IonContent>
+     </IonPage>
+   );
+
+   export default KanbanPage;
